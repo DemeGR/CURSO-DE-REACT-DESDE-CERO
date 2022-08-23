2bdf9beaf700a2f1b5f36d7545b12710dc45a533
DemeGR <gurd970714@gs.utm.mx>
2022-08-22 23:56:32 -0500

    Utilizando useEffect.

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 const GIFS = ['https://media1.giphy.com/media/xThuWdQdTh6C0BaBc4/giphy.webp?cid=ecf05e47dfgyr65hdrygsrhi5zwtmpuaguew6nmdpamo4idn&rid=giphy.webp&ct=g','https://media3.giphy.com/media/10Semkr8IesDe0/giphy.webp?cid=ecf05e47qs733uu5z36trjlyurvn4v1zwvrwmddu1cpfuvs2&rid=giphy.webp&ct=g'
@@ -10,12 +10,19 @@ const DIREFFERENT_GITS = [
 
 function App() {
   const [gifs, setGifs] = useState(GIFS)
+
+  useEffect(function (){//solo con centrarce cambiara los gifs sin necesidad de un boton 
+    console.log('actualizando los gifs')
+    setGifs(DIREFFERENT_GITS)
+  },[]/*dependencias */)//se ejecuta solo una vez porque no tiene dependencias
   
   return (
     <div className="App">
       <section className="App-content">
-      {gifs.map(singleGif => <img src={singleGif}/> )} 
-      <button onClick={()=>setGifs(DIREFFERENT_GITS)}>Cambiar gifs</button>
+      {
+        gifs.map(singleGif => <img src={singleGif}/> )
+      } 
+      <button onClick={() => setGifs(DIREFFERENT_GITS)}> Cambiar gifs </button>
       </section>
     </div>
   );
