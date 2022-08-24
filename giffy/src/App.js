41992fb0ced4cf099d624352fef35fe105f58ae7
DemeGR <gurd970714@gs.utm.mx>
2022-08-23 23:39:11 -0500

    Extrayendo lógica a un servicio. Con solo hacer una modificacion se aplica en todos los gifs.

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -1,28 +1,27 @@
 import React,{useEffect,useState} from 'react';
 import './App.css';
+import getGifs from './services/getGifs';
 
-const GIFS = ['https://media1.giphy.com/media/xThuWdQdTh6C0BaBc4/giphy.webp?cid=ecf05e47dfgyr65hdrygsrhi5zwtmpuaguew6nmdpamo4idn&rid=giphy.webp&ct=g','https://media3.giphy.com/media/10Semkr8IesDe0/giphy.webp?cid=ecf05e47qs733uu5z36trjlyurvn4v1zwvrwmddu1cpfuvs2&rid=giphy.webp&ct=g'
-]
-
-const DIREFFERENT_GITS = [
-  'https://media3.giphy.com/media/l0MYOwS7JDddcyo3m/200w.webp?cid=ecf05e47ps0ezjez2osp2z6b7ncp04iw2eoq02fwv701rau8&rid=200w.webp&ct=g'
-]
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS)
+  const [gifs, setGifs] = useState([])
 
   useEffect(function (){//solo con centrarce cambiara los gifs sin necesidad de un boton 
-    console.log('actualizando los gifs')
-    setGifs(DIREFFERENT_GITS)
+   getGifs({keyword:'rick'}).then(gifs => setGifs(gifs))
   },[]/*dependencias */)//se ejecuta solo una vez porque no tiene dependencias
   
   return (
     <div className="App">
       <section className="App-content">
       {
-        gifs.map(singleGif => <img src={singleGif}/> )
+        gifs.map(singleGif => {
+          return <div>
+            <h4>{singleGif.title}</h4>
+             <small>{singleGif.id}</small>
+             <img alt={singleGif.title} src={singleGif.url}/> 
+          </div>
+        })
       } 
-      <button onClick={() => setGifs(DIREFFERENT_GITS)}> Cambiar gifs </button>
       </section>
     </div>
   );
