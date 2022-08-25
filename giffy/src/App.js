afdd9af0a9fee3646b15928f870262b15778a041
DemeGR <gurd970714@gs.utm.mx>
2022-08-24 22:38:03 -0500

    Añadiendo reutas con wouter.

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -2,16 +2,25 @@ import React, {useState} from 'react';
 import './App.css';
 import ListOfGifs from './componentes/ListOfGifs';
 
+import {Route/*Para saber que rusta tinen nuestrs api */ } from "wouter";
 
 export default function App() {
-  const [keyword, setKeyword] = useState('panda')
-
   return (
     <div className="App">
-      <section className="App-content">
-        <button onClick={() => setKeyword('mapache')}
-        >Cambiar keyword</button>
-        <ListOfGifs keyword={keyword} />
+      <section 
+      className="App-content">
+       <h1>App</h1>
+       <a href='/gif/panda'>Gifs de 
+       pandas</a>
+       <a href='/gif/Ecuador'>Gifs de 
+       Ecuador</a>
+       <a href='/gif/Chile'>Gifs de 
+       Chile</a>
+       <a href='/gif/Mexico'>Gifs de 
+       Mexico</a>
+       <Route
+          component = {ListOfGifs}
+          path="/gif/:keyword" />
       </section>
     </div>
   );
--- giffy/src/componentes/ListOfGifs.js
+++ giffy/src/componentes/ListOfGifs.js
@@ -2,7 +2,9 @@ import getGifs from '../services/getGifs';
 import React,{useEffect,useState} from 'react';
 import Gif from "./Gif";
 
-export default function ListOfGifs({keyword}){
+export default function ListOfGifs({params}){
+    const {keyword} = params
+
     const [gifs, setGifs] = useState([])//estado inicial. No tine inicializacion 
 
     useEffect(function (){//En l aprimera renderizacion se llama a keyword 
@@ -10,12 +12,15 @@ export default function ListOfGifs({keyword}){
         .then(gifs => setGifs(gifs))
        },[keyword]/*dependencias */)// eslin-disable-line 
 
-    return gifs.map(({id, title,url}) =>
-        <Gif
-             id={id}
-              key={id}
-              title={title}
-              url={url}
-        />
-    )
+    return <div>
+    {    gifs.map(({id, title,url}) =>
+            <Gif
+                id={id}
+                key={id}
+                title={title}
+                url={url}
+            />
+        )
+    }
+    </div>
 }
