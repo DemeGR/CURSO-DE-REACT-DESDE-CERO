eb5576273d5ceffbf29cf9e97085d8883aaa695b
DemeGR <gurd970714@gs.utm.mx>
2022-08-25 00:34:56 -0500

    Usando state loading.

--- giffy/src/componentes/ListOfGifs.js
+++ giffy/src/componentes/ListOfGifs.js
@@ -4,13 +4,20 @@ import Gif from "./Gif";
 
 export default function ListOfGifs({params}){
     const {keyword} = params
-
+    const [loading, setLoading] = useState(false)
+    
     const [gifs, setGifs] = useState([])//estado inicial. No tine inicializacion 
 
     useEffect(function (){//En l aprimera renderizacion se llama a keyword 
+        setLoading(true)
         getGifs({keyword})
-        .then(gifs => setGifs(gifs))
-       },[keyword]/*dependencias */)// eslin-disable-line 
+        .then(gifs => {
+            setGifs(gifs)
+            setLoading(false)
+        })
+    },[keyword]/*dependencias */)
+
+    if(loading) return <i>Cargando @</i>
 
     return <div>
     {    gifs.map(({id, title,url}) =>
