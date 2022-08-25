425cf3818ed9a46a8be618422cf573715dd0ed53
DemeGR <gurd970714@gs.utm.mx>
2022-08-25 00:04:02 -0500

    Conseguir que sea una SPA sin recargas.

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import ListOfGifs from './componentes/ListOfGifs';
 
-import {Route/*Para saber que rusta tinen nuestrs api */ } from "wouter";
+import {Link,Route/*Para saber que rusta tinen nuestrs api */ } from "wouter";
 
 export default function App() {
   return (
@@ -10,14 +10,14 @@ export default function App() {
       <section 
       className="App-content">
        <h1>App</h1>
-       <a href='/gif/panda'>Gifs de 
-       pandas</a>
-       <a href='/gif/Ecuador'>Gifs de 
-       Ecuador</a>
-       <a href='/gif/Chile'>Gifs de 
-       Chile</a>
-       <a href='/gif/Mexico'>Gifs de 
-       Mexico</a>
+       <Link to='/gif/panda'>Gifs de 
+       pandas</Link>
+       <Link to='/gif/Ecuador'>Gifs de 
+       Ecuador</Link>
+       <Link to='/gif/Chile'>Gifs de 
+       Chile</Link>
+       <Link to='/gif/Mexico'>Gifs de 
+       Mexico</Link>
        <Route
           component = {ListOfGifs}
           path="/gif/:keyword" />
