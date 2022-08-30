1099206c5409c5cd6533cc903c3c2c43ad99d4e7
DemeGR <gurd970714@gs.utm.mx>
2022-08-29 22:01:50 -0500

    Video 4: Lazy Load, Suspense y paginación con React; Agregar título con espacios.

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -6,16 +6,18 @@ import { useGifs } from '../../hooks/useGifs';
 export default function SearchResults({params}){
 
     const {keyword} = params
-    const {loading, gifs} = useGifs({keyword})//se inicia el estado en falso
+    const {loading, gifs} = useGifs({
+        keyword})//se inicia el estado en falso
    
-    console.log({loading, gifs})
-
-
     return <>
         {loading// si carga ,se muestra el spinner. 
         //Si no se muestda lalista de  gifs
             ? <Spinner/>
-            : <ListOfGifs gifs={gifs} />
+            :<> 
+                <h3 className="App-title">{decodeURI}
+                {(keyword)}</h3>
+                <ListOfGifs gifs={gifs} />
+            </>
         }
     </>
 }
