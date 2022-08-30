37006f871e2ab22069e5f08317dcc4d1b0f45f8d
DemeGR <gurd970714@gs.utm.mx>
2022-08-30 03:24:36 -0500

    Video 4: Lazy Load, Suspense y paginación con React; ¿Qué se Suspense y porqué lo necesitamos?

--- src/componentes/TrendingSearches/index.js
+++ src/componentes/TrendingSearches/index.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React,{Suspense} from "react";
 import useNearScreen from "hooks/useNearScreen"
+import Spinner from 'componentes/Spinner'
 
 const TrendingSearches = React.lazy(
     () => import('./TrendingSearches')
 )
 
 export default function LazyTrending(){
-    const {isNearScreen, fromRef} = useNearScreen({distance: '200px'})
+const {isNearScreen, fromRef} = useNearScreen({distance: '0px'})
     
     return <div ref={fromRef}>
-        {isNearScreen ?  <TrendingSearches /> : null}
+        <Suspense fallback = {<Spinner />}>
+        {isNearScreen ?  <TrendingSearches /> :<Spinner />}
+        </Suspense>
     </div>
 }
