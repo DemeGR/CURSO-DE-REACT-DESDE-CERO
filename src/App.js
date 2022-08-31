e1b1a798a94bed396dc9a437945b54eee407f838
DemeGR <gurd970714@gs.utm.mx>
2022-08-31 00:06:38 -0500

    Video 5: CSS Grid, Infinite Scroll y Tests en nuestra app de React; auto-filt en .ListOfGifs.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import SearchResults from './pages/SearchResults'
 import Detail from './pages/Detail'
 import StaticContext from './Context/StaticContext'
+//import logo from 'logo.jpg' 
 
 import {Link,Route} from 'wouter'
 import { GifsContextProvider } from './Context/GifsContext'
@@ -17,7 +18,11 @@ export default function App() {
       <div className="App">
         <section className="App-content">
         <Link to="/"> 
-          <img className="App-logo" alt = 'Giffy logo' src = '/logo.png' />
+
+          <figure className="App-logo">
+              <img alt='Giffy logo' src='logo'/*{logo} para agregar una imagen */ />
+             
+            </figure>
         </Link>
         <GifsContextProvider>
           <Route
