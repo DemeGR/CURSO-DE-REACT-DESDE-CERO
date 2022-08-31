0dbd397b98ab119a6b80eeb55e5ac362ac874ca2
DemeGR <gurd970714@gs.utm.mx>
2022-08-31 13:39:24 -0500

    Video 5: CSS Grid, Infinite Scroll y Tests en nuestra app de React; waitForElement en testing-library.

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import {render } from '@testing-library/react';
+import React from 'react';
+import {render, waitForElement } from '@testing-library/react';
 import App from '../App';
 
-test('home work as expected', () => {
+test('home work as expected', async() => {
  const {container} = render(<App/>) 
+ const gifLink = await waitForElement(
+    () => container.querySelector('.Gif-link')
+    )
 
- const gifLink = container.querySelector('.Gif-link')
  expect(gifLink).toBeVisible()
 })
+
