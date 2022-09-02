d8df6fee40c0069e79921a79c6b1730430ad6ae0
DemeGR <gurd970714@gs.utm.mx>
2022-09-02 01:06:42 -0500

    Video 7: SEO con React y Deploy integrando con GitHub; Empezando con el SEO con useTitle.

---
+++ src/hooks/useTitle.js
@@ -0,0 +1,5 @@
+//import {useEffect} from useTitle ({title}){}
+
+export default function useTitle({title}){
+    document.title = `${title} | Giffy`
+}
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -3,10 +3,13 @@ import {Redirect} from 'wouter'
 import Gif from 'componentes/Gif'
 import useSingleGif from 'hooks/useSingleGif'
 import Spinner from 'componentes/Spinner'
+import useTitle from 'hooks/useTitle'
 
 export default function Detail({params}){
     const {gif,isLoading,isError} = useSingleGif({id: params.id})
-   
+    const title = gif?gif.title:'' //se recupera el nombre del gif
+    useTitle({title})//titulo que se muestra en la pestaña
+
     if(isLoading) return <Spinner/>
     if(isError) return <Redirect to= '404'/>//si hay un error lo direccionas al 404
     if(!gif) return null
