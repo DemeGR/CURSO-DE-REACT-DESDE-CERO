fe38eb8615a04218ef917539ed07e2052b7454bb
DemeGR <gurd970714@gs.utm.mx>
2022-09-03 20:57:36 -0500

    Video 8: useReducer y tests de hooks con react-hooks-testing-library; soportar nueva ruta de búsqueda y mantener compatibilidad.

--- src/componentes/SearchForm/index.js
+++ src/componentes/SearchForm/index.js
@@ -1,10 +1,13 @@
 import React, {useState} from "react"
 import {useLocation } from "wouter"
 
-export default function SearchForm({onSubmit})/*Funcion que se ejecutara cuando se haga submit con el boton o enter*/ {
-    const [keyword, setKeyword] = useState('')
+const RATINGS = ['g','pg','pg-13','r'];
 
-    const [path, pushLocation] = useLocation()
+export default function SearchForm()/*Funcion que se ejecutara cuando se haga submit con el boton o enter*/ {
+    const [keyword, setKeyword] = useState('')
+    const [rating, setRating] = useState(RATINGS[0])
+
+    const [, pushLocation] = useLocation()
   
     const handleSubmit = evt => {
         evt.preventDefault()//para evitar que aga su comportamiketnoo por defecto
@@ -17,10 +20,18 @@ export default function SearchForm({onSubmit})/*Funcion que se ejecutara cuando 
         setKeyword(evt.target.value)
     }
 
+    const handleChangeRating = (evt) => (
+        setRating(evt.target.value)
+    )
+
     return(//
         <form onSubmit={handleSubmit}>
                  <button>Buscar</button>
                 <input placeholder="Search a gif here..." onChange = {handleChange} type='text' value ={keyword} />  
+                <select onChange={handleChangeRating} value={rating}>
+                    <option disabled>Rating type</option>
+                    {RATINGS.map((rating) => (<option key={rating}>{rating}</option>))}
+                </select>
         </form>    
     )
 }
