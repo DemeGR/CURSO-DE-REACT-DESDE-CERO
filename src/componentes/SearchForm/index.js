d0d0ca26621315a7575fa2d18a7aa99f4c67e207
DemeGR <gurd970714@gs.utm.mx>
2022-09-03 23:45:47 -0500

    Video 8: useReducer y tests de hooks con react-hooks-testing-library;  implementando un useReducer desde cero.

--- src/componentes/SearchForm/index.js
+++ src/componentes/SearchForm/index.js
@@ -1,35 +1,45 @@
-import React, {useState} from "react"
+import React, {useState,useReducer} from "react"
 import {useLocation } from "wouter"
 
 const RATINGS = ['g','pg','pg-13','r'];
 
-export default function SearchForm({initialkeyword='',initialRating='g'})/*Funcion que se ejecutara cuando se haga submit con el boton o enter*/ {
-    const [keyword, setKeyword] = useState(decodeURIComponent(initialkeyword));
-    const [rating, setRating] = useState(initialkeyword);
-    const [times,setTimes] = useState(0)
+const reducer=(state,param) => {
+    return {
+        ...state,
+        keyword:param,
+        times:state.times +1
+    }
+}
+
+export default function SearchForm({initialKeyword='',initialRating='g'})/*Funcion que se ejecutara cuando se haga submit con el boton o enter*/ {
+    //const [keyword, setKeyword] = useState(decodeURIComponent(initialkeyword));
+   const [rating, setRating] = useState(initialRating);
+    //const [times,setTimes] = useState(0)
+
+    const [state,dispatch] = useReducer(reducer,{keyword:decodeURIComponent(initialKeyword),times: 0
+    })
+
+    const {keyword,times}=state
+
     const [, pushLocation] = useLocation();
   
     const handleSubmit = (evt) => {
-        evt.preventDefault()//para evitar que aga su comportamiketnoo por defecto
+        evt.preventDefault();//para evitar que aga su comportamiketnoo por defecto
         //navegar a otra ruta
         pushLocation(  `/search/${keyword}/${rating}`);
-        
-    }
-   ////se atualian los datos de forma imperativa. No es muy valido.
+    };
+  
     const updateKeyword = (keyword) =>{
-        setKeyword(keyword)
-        setTimes(times + 1)
+        dispatch(keyword)
     }
 
 
     const handleChange = (evt) =>{
         updateKeyword(evt.target.value)
-        setTimes(times + 1)
     }
- ////
+ 
     const handleChangeRating = (evt) => (
         setRating(evt.target.value)
-        
     )
 
     return(//
