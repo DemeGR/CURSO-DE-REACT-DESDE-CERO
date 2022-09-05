ad1feaf6e4c54dd8f79f256a75f874311a60f61a
DemeGR <gurd970714@gs.utm.mx>
2022-09-05 00:52:20 -0500

    Video 9: Login y Sesión de Usuario para gurdar Favoritos; creamos el UserContext.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import {Link,Route,Switch} from 'wouter'
 import Header from 'componentes/Header'
 
 import Login from 'pages/Login'
-import SearchResults from './pages/SearchResults'
+import SearchResults from 'pages/SearchResults'
 import Detail from './pages/Detail'
 import StaticContext from './Context/StaticContext'
-//import logo from 'logo.jpg' 
-
+import {UserContextProvider} from './Context/UserContext'
 
 import { GifsContextProvider } from './Context/GifsContext'
 
@@ -19,10 +18,7 @@ const HomePage = React.lazy(() => import('./pages/Home')
 
 export default function App() {
   return (
-    <StaticContext.Provider 
-      value={{name:'midudev',
-      suscribeteAlCanal: true }}
-    >
+    <UserContextProvider>
       <div className="App">
         <Suspense fallback={null}>
         <section className="App-content">
@@ -56,7 +52,7 @@ export default function App() {
         </section>
         </Suspense>
       </div>
-    </StaticContext.Provider>
+    </UserContextProvider>
   )
 }
 
